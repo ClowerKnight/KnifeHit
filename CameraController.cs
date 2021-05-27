5bf81795c37323a0a364efe2c5b32790c604c6f9
Furkan Tasdemir <66878884+ClowerKnight@users.noreply.github.com>
2021-05-27 10:48:39 +0300

    Update CameraController.cs

--- CameraController.cs
+++ CameraController.cs
@@ -6,7 +6,7 @@ public class CameraController : MonoBehaviour
 {
     public float speed = 1f;
     private void Update()
-       .
+       
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
        
